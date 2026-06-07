6d38ce372f7a4d36c725ad80d4fa28c774c0e4fb
Dylan Bagnawe <dylanbagnawe@gmail.com>
2026-06-07 18:02:25 +0100

    Add room management routes to chat API

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -3,19 +3,25 @@ const router  = express.Router();
 const {
   getUsers, sendMessage,
   getBroadcasts, getPrivateMessages, getUnreadCounts,
-  createRoom, getRooms, getRoomMessages
+  createRoom, getRooms, getRoomMessages,
+  updateRoom, deleteRoom, getRoomMembers, addRoomMember, removeRoomMember
 } = require('../controllers/chatController');
 const { auth } = require('../middleware/auth');
 
 router.use(auth);
 
-router.get('/users',                  getUsers);
-router.post('/send',                  sendMessage);
-router.get('/broadcast',              getBroadcasts);
-router.get('/private/:userId',        getPrivateMessages);
-router.get('/unread',                 getUnreadCounts);
-router.post('/rooms',                 createRoom);
-router.get('/rooms',                  getRooms);
-router.get('/rooms/:roomId/messages', getRoomMessages);
+router.get('/users',                          getUsers);
+router.post('/send',                          sendMessage);
+router.get('/broadcast',                      getBroadcasts);
+router.get('/private/:userId',                getPrivateMessages);
+router.get('/unread',                         getUnreadCounts);
+router.post('/rooms',                         createRoom);
+router.get('/rooms',                          getRooms);
+router.get('/rooms/:roomId/messages',         getRoomMessages);
+router.put('/rooms/:roomId',                  updateRoom);
+router.delete('/rooms/:roomId',               deleteRoom);
+router.get('/rooms/:roomId/members',          getRoomMembers);
+router.post('/rooms/:roomId/members',         addRoomMember);
+router.delete('/rooms/:roomId/members/:uid',  removeRoomMember);
 
 module.exports = router;
